e6ab541e29a98b79216e609164545f21400ce208
minaru <52421576+minaru-K@users.noreply.github.com>
2023-08-16 18:12:19 +0300

    Update app.js
    1. Added a counter of completed tasks
    2. Added closing of the modal window when clicking on the area outside the window
    3. Added the ability to sort

--- app.js
+++ app.js
@@ -19,21 +19,21 @@ const createBtn = document.getElementById("create");
 const cancelBtn = document.getElementById("cancel");
 const addBtn = document.getElementById("add");
 const inputForm = document.getElementById("inputForm");
+const sortForm = document.getElementById("sort");
+let buttonState = "";
 let indexEvent; // Для хранения индекса родительского при нажатии кнопки Редактировать
 // Не путать taskList (массив, хранящий задачи) и listTask - список элементов HTML
 const taskList = [
   {
-    title:
-      "fffffffffffffjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfffffffffffffffffffffffffffffffggggggg hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+    title: "Сегодя я проверяют",
     comment:
       "привет как дела как погода азаза мне нравятся ноги твои и глаза привет как дела как погода",
-    date: dateTask.value,
-    id: Date.now(),
+    date: "2023-08-01",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
   {
-    title:
-      "Какой-то бессмысленный набор слов, необходимый для проверки переноса текста, да-да",
+    title: "Работу тудулиста",
     comment: `Сегодня утром в новостях
     Плясали танцы на костях,
     Ругались матом при гостях.
@@ -47,21 +47,20 @@ const taskList = [
     Я хочу велосипед и чистой футболкой
     Падать-падать-падать,
     Падать в грязь, падать в грязь!`,
-    date: dateTask.value,
-    id: Date.now(),
+    date: "2023-06-23",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
   {
-    title: "Короткий набор текста",
-    comment: "",
-    date: dateTask.value,
-    id: Date.now(),
+    title: "Быть или не быть",
+    comment: "after",
+    date: "2023-09-01",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
   {
-    title:
-      "Какой-то бессмысленный набор слов, необходимый для проверки переноса текста, да-да",
-    comment: `Сегодня утром в новостях
+    title: "Акакия",
+    comment: `today утром в новостях
     Плясали танцы на костях,
     Ругались матом при гостях.
     А пару выпусков спустя
@@ -74,14 +73,13 @@ const taskList = [
     Я хочу велосипед и чистой футболкой
     Падать-падать-падать,
     Падать в грязь, падать в грязь!`,
-    date: dateTask.value,
-    id: Date.now(),
+    date: "2023-09-02",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
   {
-    title:
-      "Какой-то бессмысленный набор слов, необходимый для проверки переноса текста, да-да",
-    comment: `Сегодня утром в новостях
+    title: "Да-да?",
+    comment: `бывало утром в новостях
     Плясали танцы на костях,
     Ругались матом при гостях.
     А пару выпусков спустя
@@ -94,13 +92,13 @@ const taskList = [
     Я хочу велосипед и чистой футболкой
     Падать-падать-падать,
     Падать в грязь, падать в грязь!`,
-    date: dateTask.value,
-    id: Date.now(),
+    date: "2022-01-01",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
   {
-    title: 111111,
-    comment: `Сегодня утром в новостях
+    title: "Бывало и лучше",
+    comment: `пока утром в новостях
     Плясали танцы на костях,
     Ругались матом при гостях.
     А пару выпусков спустя
@@ -113,13 +111,13 @@ const taskList = [
     Я хочу велосипед и чистой футболкой
     Падать-падать-падать,
     Падать в грязь, падать в грязь!`,
-    date: dateTask.value,
-    id: Date.now(),
+    date: "2023-02-04",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
   {
-    title: 111111,
-    comment: `Сегодня утром в новостях
+    title: "HEllo",
+    comment: `аааа утром в новостях
     Плясали танцы на костях,
     Ругались матом при гостях.
     А пару выпусков спустя
@@ -132,23 +130,20 @@ const taskList = [
     Я хочу велосипед и чистой футболкой
     Падать-падать-падать,
     Падать в грязь, падать в грязь!`,
-    date: dateTask.value,
-    id: Date.now(),
+    date: "2023-08-05",
+    id: Date.now() + Math.random() * 100,
     state: false,
   },
 ];
 
-renderTasks(taskList);
-// //syncJson()
-// let tasksJson
-// function syncToJson() {
-//   tasksJson = JSON.stringify(taskList)
-//   console.log('таска', tasksJson)
-// }
+sortForm.innerHTML = `<option value="new">Новые</option>
+<option value="old">Старые</option>
+<option value='date'>По дате</option>
+<option value='title'>По названию</option>
+<option value='comment'>По описанию</option>`;
 
-// function syncToTaskList() {
-//   //Основная проблема - у js нет доступа к ФС пользователя. Я не могу сохранить файл json в системе и подхватить данные из него
-// }
+renderTasks(taskList);
+counterTasks(taskList);
 
 function renderTasks(arrTask) {
   listTask.innerHTML = "";
@@ -183,9 +178,9 @@ function getTaskList(arrTask, index) {
   <tr>
     <td>Статуc: ${arrTask.state === false ? "Не выполнено" : "Выполнено"}</td>
   </tr>
-   <tr><td class="buttonsStateDelete"><input type="submit" data-index="${index}" value="Выполнено">
-  <input type="reset" data-index="${index}" value="Удалить">
-  <input type="button" data-index="${index}" value="Редактировать"></td></tr>
+   <tr><td class="buttonsStateDelete"><input type="submit"  ${buttonState} data-index="${index}" value="Выполнено" id="ready">
+  <input type="reset" data-index="${index}" ${buttonState} value="Удалить" id="delete">
+  <input type="button" data-index="${index}" ${buttonState} value="Редактировать" id="edit"></td></tr>
   </tbody> <br>`;
   // `<li>${
   //   arrTask.state === true ? document.getElementsByClassName("taskList")[0].style = "text-decoration: line-through;" :  document.getElementsByClassName("taskList")[0].style = "text-decoration: '';"
@@ -217,7 +212,79 @@ function getTaskList(arrTask, index) {
   // </tr>`;
 }
 
-listTask.onclick = function (event) {
+function buttonDisabled() {
+  buttonState = "disabled";
+  sortForm.disabled = true;
+  addBtn.disabled = true;
+  document.getElementsByClassName("sortForm")[0].style.cursor = "default";
+  document.getElementsByClassName("buttonsStateDelete")[0].style.cursor =
+    "default";
+  document.getElementsByClassName("buttonsRightCreateTask")[0].style.cursor =
+    "default";
+  renderTasks(taskList);
+}
+
+function buttonEnabled() {
+  buttonState = "";
+  sortForm.disabled = false;
+  addBtn.disabled = false;
+  document.getElementsByClassName("sortForm")[0].style.cursor = "pointer";
+  document.getElementsByClassName("buttonsStateDelete")[0].style.cursor =
+    "pointer";
+  document.getElementsByClassName("buttonsRightCreateTask")[0].style.cursor =
+    "pointer";
+  renderTasks(taskList);
+}
+
+function counterTasks(arrTask) {
+  let readyTask = 0;
+  for (let i = 0; i < arrTask.length; i++) {
+    if (arrTask[i].state === true) {
+      readyTask += 1;
+    }
+  }
+  document.getElementById("counter").innerHTML = renderCounter(
+    arrTask.length,
+    readyTask
+  );
+}
+
+function renderCounter(all, ready) {
+  return `Всего задач: ${all} <br>
+          Выполнено: ${ready}`;
+}
+
+sortForm.addEventListener("change", function () {
+  switch (this.value) {
+    case "new": {
+      taskList.sort((a, b) => (a.id > b.id ? -1 : 1));
+      break;
+    }
+    case "old": {
+      taskList.sort((a, b) => (a.id > b.id ? 1 : -1));
+      break;
+    }
+    case "date": {
+      taskList.sort((a, b) => (a.date > b.date ? 1 : -1));
+      break;
+    }
+    case "title": {
+      taskList.sort((a, b) =>
+        a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
+      );
+      break;
+    }
+    case "comment": {
+      taskList.sort((a, b) =>
+        a.comment.toLowerCase() > b.comment.toLowerCase() ? 1 : -1
+      );
+      break;
+    }
+  }
+  renderTasks(taskList);
+});
+
+listTask.addEventListener("click", function (event) {
   if (event.target.dataset.index) {
     const index = parseInt(event.target.dataset.index);
     const type = event.target.type;
@@ -229,33 +296,70 @@ listTask.onclick = function (event) {
       redactTask(index);
       indexEvent = index;
     }
+    event.stopPropagation();
+    counterTasks(taskList);
     renderTasks(taskList);
   }
-};
+});
 
 function redactTask(index) {
-  document.getElementsByClassName("redactTaskWindow")[0].style =
-    "z-index: 3000";
+  document.getElementsByClassName("redactTaskWindow")[0].style.zIndex = "2000";
   document.getElementById("titleRedact").value = taskList[index].title;
   document.getElementById("commentRedact").value = taskList[index].comment;
   document.getElementById("dateRedact").value = taskList[index].date;
+  document.getElementsByClassName("mainMenu")[0].style.overflow = "hidden";
+  buttonDisabled();
 }
 
 document.getElementById("cancelRedact").onclick = () => {
-  document.getElementsByClassName("redactTaskWindow")[0].style = "z-index: 0";
+  document.getElementsByClassName("redactTaskWindow")[0].style.zIndex = "0";
+  document.getElementsByClassName("mainMenu")[0].style.overflow = "";
+  buttonEnabled();
 };
 
 document.getElementById("save").onclick = (event) => {
-  taskList[indexEvent].title = document.getElementById("titleRedact").value;
-  taskList[indexEvent].comment = document.getElementById("commentRedact").value;
-  taskList[indexEvent].date = document.getElementById("dateRedact").value;
-  document.getElementsByClassName("redactTaskWindow")[0].style = "z-index: 0";
-  renderTasks(taskList);
+  if (
+    document.getElementById("titleRedact").value === "" ||
+    document.getElementById("dateRedact").value === ""
+  ) {
+    alert("Некорректный ввод данных!");
+  } else {
+    taskList[indexEvent].title = document.getElementById("titleRedact").value;
+    taskList[indexEvent].comment =
+      document.getElementById("commentRedact").value;
+    taskList[indexEvent].date = document.getElementById("dateRedact").value;
+    document.getElementsByClassName("redactTaskWindow")[0].style.zIndex = "0";
+    buttonEnabled();
+    // renderTasks(taskList)
+    document.getElementsByClassName("mainMenu")[0].style.overflow = "";
+  }
 };
+
+const divInputForm = document.querySelector("#inputForm");
+const divEditTask = document.querySelector("#editForm");
+document.addEventListener("click", (e) => {
+  const withinBoundaries = e.composedPath().includes(divEditTask);
+  const withinBoundaries2 = e.composedPath().includes(divInputForm);
+  if (
+    (!withinBoundaries &&
+      document.getElementsByClassName("redactTaskWindow")[0].style.zIndex !=
+        "0") ||
+    (!withinBoundaries2 &&
+      document.getElementsByClassName("inputForm")[0].style.zIndex != "0")
+  ) {
+    document.getElementsByClassName("redactTaskWindow")[0].style.zIndex = "0";
+    document.getElementsByClassName("inputForm")[0].style.zIndex = "0";
+    document.getElementsByClassName("mainMenu")[0].style.overflow = "";
+    buttonEnabled();
+    cleanInput();
+  }
+});
 
 cancelBtn.onclick = () => {
   cleanInput();
-  document.getElementsByClassName("inputForm")[0].style = "z-index: 0";
+  buttonEnabled();
+  document.getElementsByClassName("inputForm")[0].style.zIndex = "0";
+  document.getElementsByClassName("mainMenu")[0].style.overflow = "";
 };
 
 function cleanInput() {
@@ -265,19 +369,28 @@ function cleanInput() {
 }
 
 createBtn.onclick = () => {
-  taskList.push({
-    title: titleTask.value,
-    comment: commentTask.value,
-    date: dateTask.value,
-    id: Date.now(),
-    state: false,
-  });
-  cleanInput();
-  renderTasks(taskList);
-  document.getElementsByClassName("inputForm")[0].style = "z-index: 0";
-  //syncJson()
+  if (titleTask.value === "" || dateTask === "") {
+    alert("Некорректный ввод данных!");
+  } else {
+    taskList.unshift({
+      title: titleTask.value,
+      comment: commentTask.value,
+      date: dateTask.value,
+      id: Date.now(),
+      state: false,
+    });
+    cleanInput();
+    buttonEnabled();
+    // renderTasks(taskList);
+    document.getElementsByClassName("inputForm")[0].style.zIndex = "0";
+    document.getElementsByClassName("mainMenu")[0].style.overflow = "";
+    counterTasks(taskList);
+  }
 };
 
-addBtn.onclick = () => {
-  document.getElementsByClassName("inputForm")[0].style = "z-index: 2000";
+addBtn.onclick = (event) => {
+  document.getElementsByClassName("inputForm")[0].style.zIndex = "2000";
+  document.getElementsByClassName("mainMenu")[0].style.overflow = "hidden";
+  buttonDisabled();
+  event.stopPropagation();
 };
